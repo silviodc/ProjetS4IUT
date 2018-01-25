a22c34499b777bb8453a568ce212cc74ad3928e9
Silvio Domingos <silvio.d.cardoso@gmail.com>
2018-01-25 13:27:40 +0100

    Update runCompose.sh

--- javascript/runCompose.sh
+++ javascript/runCompose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export http_proxy=http://username:password@proxy.com:port
-export https_proxy=http://username:password@proxy.com:port
-export no_proxy=localhost,127.0.0.1,company.com
+export http_proxy=http://proxy.iut-orsay.fr:3128
+export https_proxy=https://proxy.iut-orsay.fr:3128
+export no_proxy=localhost,127.0.0.1
 docker-compose up
